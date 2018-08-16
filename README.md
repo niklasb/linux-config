11d589b2f684283b2791d1f5dd121482d944b261
Niklas Baumstark <niklas.baumstark@gmail.com>
2018-08-16 10:50:51 +0200

    stuff

---
+++ README.md
@@ -0,0 +1,3 @@
+```
+curl https://raw.githubusercontent.com/niklasb/linux-config/master/install.sh | bash
+```
--- reset.sh
+++ reset.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 cd ~
-rm -rf .vim .vimrc vimrc gdbinit .zshrc linux-config
+rm -rf .vim .vimrc vimrc gdbinit .gdbinit .zshrc linux-config
