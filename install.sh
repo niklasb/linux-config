4ff3b68c2805f693d2ff53a2101dbe31b1478e4c
Niklas Baumstark <niklas.baumstark@gmail.com>
2020-08-17 23:25:58 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@ cd ~
 if which apt-get; then
   sudo apt-get update -y
   sudo apt-get install -y curl git vim exuberant-ctags zsh gdb silversearcher-ag \
-    python-minimal python-virtualenv python-pip \
     python3-minimal python3-virtualenv python3-pip
 elif which pacman; then
   sudo pacman -Sy gvim python python2 python-pip python2-pip ctags gdb curl git \
